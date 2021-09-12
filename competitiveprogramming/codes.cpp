fd082a64cbc993188108f60e12ceddd151659cb0
jack tan <62325379+jacktan130802@users.noreply.github.com>
2021-09-12 23:26:57 +0800

    got error conditional for loops

--- competitiveprogramming/codes.cpp
+++ competitiveprogramming/codes.cpp
@@ -2,6 +2,32 @@
 #include <cstdio>
 #include <iomanip>
 using namespace std;
+ void odd(int x) {
+     if (x % 2 != 0) {
+         cout << "odd" << endl ;
+     }
+     //else even
+     else
+         cout << "even" <<endl; 
+}
+ void toString(int x) {
+     switch (x) {
+     case 0: cout << "zero"<<endl;break;
+     case 1: cout << "one"<<endl;break;
+     case 2: cout << "two"<<endl;break;
+     case 3: cout << "three"<<endl;break;
+     case 4: cout << "four"<<endl;break;
+     case 5: cout << "five"<<endl;break;
+     case 6: cout << "six"<<endl;break;
+     case 7: cout << "seven"<<endl;break;
+     case 8: cout << "eight"<<endl;break; 
+     default: cout << "nine"<<endl;break;
+       
+     }
+ }
+ void morethan9(int x) {
+        
+ }
 int main()
 {
     /***********************Basic types****************************/
@@ -30,9 +56,26 @@ int main()
 
     /***********************Conditional loops****************************/
 
-
-
-
+    int x, x1; 
+    cin >> x;
+    cin >> x1;
+    bool num1, num2 = false;
+    //if (x <= 9)
+    //    num1 = true;
+    //if (x1 <= 9)
+    //    num2 = true;
+    ////num1 less than 9
+    //if (!num1) {
+    //    x = 9;
+    //}
+    ////num2 less than 9
+    //if (!num2) {
+    //    x1 = 9;
+    //}
+    toString(x);
+    toString(x1);
+    odd(x);
+    odd(x1);
 
 
     return 0;
