e224f76cdf0df28021a82cb2369bb3cc4914bbdc
jack tan <62325379+jacktan130802@users.noreply.github.com>
2021-09-12 15:40:10 +0800

    Add project files.

---
+++ competitiveprogramming/Source.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <cstdio>
+#include <iomanip>
+using namespace std;
+int main()
+{
+    /*Basic types*/
+
+    int integer;
+    long long1;//by default int long
+    char character;
+    float floatnumber;
+    double doublenumber;
+    cin >> integer;
+    cin >> long1;
+    cin >> character;
+    cin >> floatnumber;
+    cin >> doublenumber;
+    cout << integer << endl;
+    cout << long1 << endl;
+    cout << character << endl;
+    //*********************Using fixed************************//
+    //cout << fixed << floatnumber << setprecision(3) << endl;
+    //cout << fixed << doublenumber << setprecision(9) << endl; //Fixed for precision. How to do scanf? 
+
+
+    scanf("% .3f", &floatnumber);
+
+
+
+
+
+
+
+
+
+
+    return 0;
+}
