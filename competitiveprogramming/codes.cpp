bdf6438ebd23257d6987ff5b69d8276df437f731
jack tan <62325379+jacktan130802@users.noreply.github.com>
2021-09-12 22:42:52 +0800

    comment out day1

--- competitiveprogramming/codes.cpp
+++ competitiveprogramming/codes.cpp
@@ -4,31 +4,31 @@
 using namespace std;
 int main()
 {
-    /*Basic types*/
+    /***********************Basic types****************************/
 
-    int integer;
-    long long1;//by default int long
-    char character;
-    float floatnumber;
-    double doublenumber;
-    cin >> integer;
-    cin >> long1;
-    cin >> character;
-    cin >> floatnumber;
-    cin >> doublenumber;
-    cout << integer << endl;
-    cout << long1 << endl;
-    cout << character << endl;
-    //*********************Using fixed************************//
-    cout << fixed << floatnumber << setprecision(3) << endl;
-    cout << fixed << doublenumber << setprecision(9) << endl; //Fixed for precision. How to do scanf? 
+    //int integer;
+    //long long1;//by default int long
+    //char character;
+    //float floatnumber;
+    //double doublenumber;
+    //cin >> integer;
+    //cin >> long1;
+    //cin >> character;
+    //cin >> floatnumber;
+    //cin >> doublenumber;
+    //cout << integer << endl;
+    //cout << long1 << endl;
+    //cout << character << endl;
+    ////*********************Using fixed************************//
+    //cout << fixed << floatnumber << setprecision(3) << endl;
+    //cout << fixed << doublenumber << setprecision(9) << endl; //Fixed for precision. How to do scanf? 
 
-
+    //C libs
     /*  printf("% .3f", floatnumber);*/
 
 
 
-
+    /***********************Conditional loops****************************/
 
 
 
